758bbbbc4f82e757c5018b182775c30a7eaf49a2
Thiwanka-lab <thiwanka.sir@gmail.com>
2024-10-16 00:35:41 +0300

    update

--- main.py
+++ main.py
@@ -11,4 +11,50 @@ import os
 
 engine = pyttsx3.init('sapi5')
 voices= engine.getProperty('voices')
-print(voices[1].id)
+
+#print(voices[1].id)
+
+engine.setProperty('voice',voices[0].id)
+engine.setProperty('rate', 140)
+
+# speak function
+
+def speak(text):
+    engine.say(text)
+    engine.runAndWait()
+
+#speak("Thiwanka, how are you")
+
+
+# Speech recognition function
+
+def takeCommand():
+    """This function will recognize voice and return text"""
+
+    r = sr.Recognizer()
+    with sr.Microphone() as source:
+        print ("Listening.....")
+        r.pause_threshold = 1
+        audio = r.listen(source)
+
+        try:
+
+            print("Recognizing.....")
+            query = r.recognize_google(audio, language='en') 
+            print(f"user said: {query}\n")
+
+        except Exception as e:
+            print("Say that again please...")
+            return "None"
+        
+        return query
+
+
+# takeCommand()
+
+"""Also we can create a logic to speak what i am saying"""
+text = takeCommand()
+speak(text)
+
+#or
+#speak(takeCommand())
