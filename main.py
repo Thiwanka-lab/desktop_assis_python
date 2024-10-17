91403742815e9018cd1a98436973a6152fef6a99
Thiwanka-lab <thiwanka.sir@gmail.com>
2024-10-17 16:08:59 +0300

    update

--- main.py
+++ main.py
@@ -76,23 +76,30 @@ def takeCommand():
 """This is a logic where I can search wikipedia according to the query """
 if __name__ == "__main__":
     
-    query = takeCommand().lower()
-    #print(query)
+    wish_me()
 
-    if "wikipedia" in query:
-        query = query.replace("wikipedia","")
-        #print (query)
-        result = wikipedia.summary(query, sentences = 2)
-        
-        speak("according to wikipedia")
-        print(result)
-        speak(result)
+    while True:
 
-    elif "youtube" in query:
-        speak("Opening Youtube")
-        webbrowser.open("youtube.com") 
-    
-    elif "github" in query:
-        speak("Opening github")
-        webbrowser.open("github.com")
+        query = takeCommand().lower()
+        #print(query)
+
+        if "wikipedia" in query:
+            query = query.replace("wikipedia","")
+            #print (query)
+            result = wikipedia.summary(query, sentences = 2)
+            
+            speak("according to wikipedia")
+            print(result)
+            speak(result)
+
+        elif "youtube" in query:
+            speak("Opening Youtube")
+            webbrowser.open("youtube.com") 
         
+        elif "github" in query:
+            speak("Opening github")
+            webbrowser.open("github.com")
+            
+        elif "goodbye" in query:
+            speak("Ok sir. Thank you. Bye bye ")
+            exit()
